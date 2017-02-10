abdaaaa8c3113f9b94bee4272058026bb0a3c166
Wesley Pendleton <wesley.pendleton@ZIRMED.COM>
2017-02-10 14:49:41 -0500

    Add project files.

---
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+
+            var httpMessage = await client.GetAsync("http://apress.com");
+
+            // we could do other things here while we are waiting for the HTTP request to complete
+            return httpMessage.Content.Headers.ContentLength;
+        }
+        /* replaced by above
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+
+            var httpTask = client.GetAsync("http://apress.com");
+
+            // we could do other things here while we are waiting for the HTTP request to complete
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+            {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+        */
+    }
+}
---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,45 @@
+﻿// The purpose of this is to demonstrate using an extension method,
+// which adds functionality to an accessable class that cannot be modified
+// Assume that the total value of the Product objects in the ShoppingCart class is needed,
+// and the class cannot be modified for whatever reason
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this /*ShoppingCart cartParam*/ IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+            foreach (Product prod in /*cartParam.Products*/ productEnum)
+            {
+                total += prod.Price;
+            }
+            return total;
+        }
+        public static IEnumerable<Product> FilterByCategory
+            (this IEnumerable<Product> productEnum, string categoryParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (prod.Category == categoryParam)
+                {
+                    yield return prod;
+                }
+            }
+        }
+        public static IEnumerable<Product> Filter(this IEnumerable<Product> productEnum, Func<Product, bool> selectorParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (selectorParam(prod))
+                {
+                    yield return prod;
+                }
+            }
+        }
+    }
+}
